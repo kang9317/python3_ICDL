69ee806caece3a315bf31f162c6f4175be7075da
kang9317 <kkang9317@gmail.com>
2017-11-21 20:11:16 +0900

    Create example_9.py

---
+++ 2day/example_9.py
@@ -0,0 +1,13 @@
+print(chr(97))
+
+print("%c" % 97)
+
+print(ord('한'))
+
+print(chr(54620))
+
+print('한'.encode('utf-8'))
+
+print(b'\xed\x95\x9c'.decode('utf-8'))
+
+print(str(b'\xed\x95\x9c',encoding = 'utf-8'))
