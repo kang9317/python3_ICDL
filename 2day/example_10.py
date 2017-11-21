e19dd53035abc8cc56c09afbb2e9c6be533d58fa
kang9317 <kkang9317@gmail.com>
2017-11-21 20:43:52 +0900

    Update example_10.py

--- 2day/example_10.py
+++ 2day/example_10.py
@@ -1,56 +1,96 @@
-print(len('abcde'))
-
 a = 'abcd'
 b = 'ABCD'
 c = 'AbcD'
-a.isupper()
-
-a.islower()
-
-b.isupper()
-
-b.islower()
-
-c.isupper()
-
-c.islower()
-
+hangul = '%'
 d = 'abc'
 e = '123'
 f = 'a1b2c3'
+g = ''
+h = ' '
+i = 'Abc'
 
+print('='*10)
+print('len')
+print('='*10)
+print(len('abcde'))
+print('')
+print('='*10)
+print('isupper')
+print('='*10)
+print(a.isupper())
+print('')
+print(b.isupper())
+print('')
+print(c.isupper())
+
+print('')
+print('='*10)
+print('islower')
+print('='*10)
+print(a.islower())
+print('')
+print(b.islower())
+print('')
+print(c.islower())
+
+print('')
+print('='*10)
+print('isalnum')
+print('='*10)
 print(d.isalnum())
 print('')
 print(e.isalnum())
 print('')
 print(f.isalnum())
+print('')
+print(hangul.isalnum())
 
+print('')
+print('='*10)
+print('isalpha')
+print('='*10)
 print(d.isalpha())
 print('')
 print(e.isalpha())
 print('')
 print(f.isalpha())
 
+print('')
+print('='*10)
+print('isdecimal')
+print('='*10)
 print(d.isdecimal())
 print('')
 print(e.isdecimal())
 print('')
 print(f.isdecimal())
 
-print(d.isprintable())
 print('')
-print(e.isprintable())
+print('='*10)
+print('isprintable')
+print('='*10)
+print(g.isprintable())
 print('')
-print(f.isprintable())
+print(h.isprintable())
+print('')
+print(i.isprintable())
 
-print(d.isspace())
 print('')
-print(e.isspace())
+print('='*10)
+print('isspace')
+print('='*10)
+print(g.isspace())
 print('')
-print(f.isspace())
+print(h.isspace())
+print('')
+print(i.isspace())
 
-print(d.istitle())
 print('')
-print(e.istitle())
+print('='*10)
+print('istitle')
+print('='*10)
+print(g.istitle())
 print('')
-print(f.istitle())
+print(h.istitle())
+print('')
+print(i.istitle())
