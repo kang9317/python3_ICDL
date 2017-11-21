5b85b32cdeaf4c5fd1cee7103e2aeb87121b7bfb
kang9317 <kkang9317@gmail.com>
2017-11-21 20:31:22 +0900

    Update example_10.py

--- 2day/example_10.py
+++ 2day/example_10.py
@@ -14,3 +14,43 @@ b.islower()
 c.isupper()
 
 c.islower()
+
+d = 'abc'
+e = '123'
+f = 'a1b2c3'
+
+print(d.isalnum())
+print('')
+print(e.isalnum())
+print('')
+print(f.isalnum())
+
+print(d.isalpha())
+print('')
+print(e.isalpha())
+print('')
+print(f.isalpha())
+
+print(d.isdecimal())
+print('')
+print(e.isdecimal())
+print('')
+print(f.isdecimal())
+
+print(d.isprintable())
+print('')
+print(e.isprintable())
+print('')
+print(f.isprintable())
+
+print(d.isspace())
+print('')
+print(e.isspace())
+print('')
+print(f.isspace())
+
+print(d.istitle())
+print('')
+print(e.istitle())
+print('')
+print(f.istitle())
