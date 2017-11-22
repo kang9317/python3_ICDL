8c4b62fab9da9ab8c6bd1dd22eac5b876b6a1120
kang9317 <kkang9317@gmail.com>
2017-11-22 19:16:35 +0900

    Create example_4.py

---
+++ 3day/example_4.py
@@ -0,0 +1,15 @@
+','.join(['a','b','c'])
+
+'xxx.mp3'.partition('.')
+
+'1,2,3'.split(',')
+
+'1,2,3'.split(',', maxsplit=1)
+
+'1,2,,3,'.split(',')
+
+'1 2 3'.split()
+
+'1 2 3'.split(maxsplit = 1)
+
+'  1  2  3  '.split()
