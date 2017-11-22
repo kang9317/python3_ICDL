5a4c141ba7b03f2465eb7e68902d70aea268f387
kang9317 <kkang9317@gmail.com>
2017-11-22 19:56:26 +0900

    Create example_9.py

---
+++ 3day/example_9.py
@@ -0,0 +1,14 @@
+'  test  '.strip()
+
+'[[test]]'.strip('[]')
+
+'  spacious  '.rstrip()
+
+'mississippi'.rstrip('ipz')
+
+'  spacious  '.lstrip()
+
+'www.example.com'.lstrip('cmowz.')
+
+a = 'Life is too short'
+a.replace('short','long')
