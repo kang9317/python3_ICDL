6233becda2e815541086180a758b07d55f57d288
kang9317 <kkang9317@gmail.com>
2017-11-22 19:25:00 +0900

    Create example_5.py

---
+++ 3day/example_5.py
@@ -0,0 +1,11 @@
+'ab c\n\nde fg\rkl\r\n'.splitlines()
+
+'ab c\n\nde fg\rkl\r\n'.splitlines(keepends = True)
+
+"".splitlines()
+
+"One line\n".splitlines()
+
+''.split('\n')
+
+'Two lines\n'.split('\n')
