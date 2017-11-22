524b42547674aec4c9f907d3ae51701b61da3671
kang9317 <kkang9317@gmail.com>
2017-11-22 20:12:35 +0900

    Create example_11.py

---
+++ 3day/example_11.py
@@ -0,0 +1,9 @@
+print("%d" % (100,))
+print("%20d" % (100,))
+print("%020d" % (100,))
+print("%-20d" % (100,))
+
+import math
+print("%f" %(math.pi,))
+print("%2f" %(math.pi,))
+print("%.3f" %(math.pi,))
