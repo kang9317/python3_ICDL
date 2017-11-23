f4c759fddab93d9d23cd2c332edb972dd784d0cf
kang9317 <kkang9317@gmail.com>
2017-11-23 19:14:36 +0900

    Create example_6.py

---
+++ 4day/example_6.py
@@ -0,0 +1,29 @@
+list(range(10))
+
+list(range(1,11))
+
+list(range(0,30,5))
+
+list(range(0,10,3))
+
+list(range(0,-10,-1))
+
+list(range(0))
+
+list(range(1,0))
+
+r = range(0,20,2)
+
+r
+
+11 in r
+
+10 in r
+
+r.index(10)
+
+r[5]
+
+r[:5]
+
+r[-1]
