06b770f8caa9d64725e166e7cee0546f0ff97d07
kang9317 <kkang9317@gmail.com>
2017-11-23 18:47:18 +0900

    Create example_3.py

---
+++ 4day/example_3.py
@@ -0,0 +1,17 @@
+data = [12,35,32,5,11]
+
+data.sort()
+print(data)
+
+data.sort(reverse=True)
+print(data)
+
+================================
+
+data=[12,35,32,5,11]
+
+sorted_data = sorted(data)
+print(sorted_data)
+
+reversed_data = sorted(data, reverse=True)
+print(reversed_data)
