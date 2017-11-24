f054193e8f012fc82dc67c821a8c5d1debb2aa7a
kang9317 <kkang9317@gmail.com>
2017-11-24 20:30:16 +0900

    Create example_12.py

---
+++ 5day/example_12.py
@@ -0,0 +1,26 @@
+from pprint import pprint
+import pickle
+
+person1 = {
+    'name': '김하나',
+    'height': 170,
+    'weight': 60
+}
+person2 = {
+    'name': '이대호',
+    'height': 200,
+    'weight': 80
+}
+
+# 데이터를 리스트로 만들었다.
+people = [person1,person2]
+
+# 데이터를 저장한다.
+with open('poeple.pickle', 'wb') as f:
+    pickle.dump(people,f)
+    
+# 저장된 데이터를 읽는다.
+with open('poeple.pickle', 'rb') as f:
+    loaded_people = pickle.load(f)
+    
+pprint(loaded_people)
