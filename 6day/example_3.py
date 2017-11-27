32e7f0361c81b09255391734b3d171c1639363c1
kang9317 <kkang9317@gmail.com>
2017-11-27 19:05:57 +0900

    Create example_3.py

---
+++ 6day/example_3.py
@@ -0,0 +1,15 @@
+sql="""
+    INSERT INTO Student VALUES
+    (?,?,?)
+"""
+c = conn.cursor()
+c.execute(sql,('학생2',2,'서울'))
+
+data = [
+    ('학생3',3,'서울'),
+    ('학생4',4,'서울'),
+    ('학생5',5,'서울'),
+]
+c.executemany(sql,data)
+c.close()
+conn.commit()
