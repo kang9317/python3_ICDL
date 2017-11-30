46b83815c449d9ff4ff031f5e53fe37b622e5afb
kang9317 <kkang9317@gmail.com>
2017-11-30 19:35:34 +0900

    Create example_10.py

---
+++ 9day/example_10.py
@@ -0,0 +1,35 @@
+st = 'World Cup Korea'
+print(st[0])
+
+lst = [1,3,5,7,9]
+print(lst[1])
+
+tpl= ('HGD', 'GJ', 19, '010-1122-3344')
+print(tpl[-1])
+
+lst[0] = 0
+print(lst)
+
+st2 = st[0:5]
+print(st2)
+
+lst2 = lst[2:]
+print(lst2)
+
+tpl2 = tpl[:3]
+print(tpl2)
+
+st2 = st[::2]
+print(st2)
+
+st2 = st +' Fighting'
+print(st2)
+
+lst2 = lst + [11,13]
+print(lst2)
+
+tpl2 = tpl+('C','Python')
+print(tpl2)
+
+lst2 = lst * 2
+print(lst2)
