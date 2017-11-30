5cf901076280d5f19da379a33de5ef90978a394f
kang9317 <kkang9317@gmail.com>
2017-11-30 19:27:43 +0900

    Create example_6.py

---
+++ 9day/example_6.py
@@ -0,0 +1,9 @@
+passWD = int(2323)
+# Update the code to ask the user password
+
+userWD = int(input("비밀번호를 입력하세요(4자리):"))
+
+if(userWD == passWD):
+    print("비밀번호가 맞습니다")
+else:
+    print("비밀번호가 틀렸습니다.")
