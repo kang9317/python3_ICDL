a96efab6ba461541aaf72ba5cda0636cd4c966e7
kang9317 <kkang9317@gmail.com>
2017-11-30 19:09:48 +0900

    Create example_1.py

---
+++ 9day/example_1.py
@@ -0,0 +1,15 @@
+s = 'Korea'
+print(list(s))
+print(tuple(s))
+
+###################################
+
+k=('a','b','c')
+v=[1,2,3]
+print(dict(zip(k,v)))
+print(str(k))
+
+###################################
+
+print(chr(44033))
+print(ord('A'))
