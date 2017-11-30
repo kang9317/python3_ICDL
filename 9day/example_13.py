13fa635966755d5a371cc3b113f17b90ca0e8520
kang9317 <kkang9317@gmail.com>
2017-11-30 20:21:40 +0900

    Create example_13.py

---
+++ 9day/example_13.py
@@ -0,0 +1,25 @@
+# Update the code here
+age = 15
+
+if age >= 65:
+    money = 0
+elif age >= 20:
+    money = 500
+elif age >= 8:
+    money =300
+else:
+    money =0
+print(age, '', money)
+
+
+########################################################
+
+# Update the code here
+age = 15
+if ((age >= 65) or (age < 8)):
+    money = 0
+elif age >= 20:
+    money = 500
+else:
+    money = 300
+print(age, '', money)
