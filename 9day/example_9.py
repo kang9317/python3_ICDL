ffa9d182d936f3327c340a29f9b83e7ab10d19ce
kang9317 <kkang9317@gmail.com>
2017-11-30 19:28:28 +0900

    Create example_9.py

---
+++ 9day/example_9.py
@@ -0,0 +1,8 @@
+country = "Korea"
+# Update the code here to ask for the users country
+myCountry = input("당신의 나라를 영어로 입력하세요: ")
+
+if(country == myCountry):
+    print("당신은 한국인입니다.")
+else:
+    print("당신은 외국인입니다.")
