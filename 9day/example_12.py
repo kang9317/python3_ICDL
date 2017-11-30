33c1f68de8134afd77d985a5c2e909f17b3183fe
kang9317 <kkang9317@gmail.com>
2017-11-30 20:09:45 +0900

    Create example_12.py

---
+++ 9day/example_12.py
@@ -0,0 +1,16 @@
+x = 3
+y = 5
+print("x > y: ",x > y)
+print("x < y: ",x < y)
+print("x == y: ", x == y)
+print("x != y: ",x != y)
+
+print('Korea' > 'korea')
+
+print((1,3,5) < (2,3,5))
+
+print([1,3,5] == [1,5,3])
+
+print(x==3 and y>3)
+print(x!=3 or y>3)
+print(not x ==3)
