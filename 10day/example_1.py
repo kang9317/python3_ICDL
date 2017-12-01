278ea5961ef74157fcfe473afe3e3c326d25070e
kang9317 <kkang9317@gmail.com>
2017-12-01 18:42:43 +0900

    Create example_1.py

---
+++ 10day/example_1.py
@@ -0,0 +1,13 @@
+a = int(input('a = '))
+b = int(input('b = '))
+result = 0
+
+def mulAB(a,b):
+    result = a * b
+    print(result)
+def divAB(a,b):
+    result = a / b
+    print(result)
+    
+mulAB(a,b)
+divAB(a,b)
